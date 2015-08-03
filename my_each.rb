5e0585300c49e7dbeb67fbf56518dfb6e57170eb
changamanda <amanda.chang10@gmail.com>
2015-08-03 16:04:58 -0400

    Code Review

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,23 @@
 def my_each(words)
   counter = 0
-  empty_array = []
-    while counter < words.length
-    new_array = yield words[counter]
+  while counter < words.length
+    yield words[counter]
     counter += 1
-    empty_array << new_array 
   end
   words
 end
+
+# ["a", "b", "c"].each do |letter|
+#   puts letter
+# end
+
+# def method
+#   line
+#   a line
+#   if xyz
+#     inside 2 spaces
+#     block.each do |args|
+#       inside some more
+#     end
+#   end
+# end
